d8482372d1174442454de5c30fb581f0d0650809
bhutani <nbhutani2@dons.usfca.edu>
2019-10-27 10:03:14 -0700

    game first draft minor fix

--- src/main/java/com/example/appengine/java8/MasterMindServlet.java
+++ src/main/java/com/example/appengine/java8/MasterMindServlet.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
 import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "MasterMindServlet", value = "/mastermind")
